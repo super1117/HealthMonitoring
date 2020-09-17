ce73843b4916a641a6d4c5ff6eacde66301c9cec
songzl <zerosong1117@gmail.com>
2020-09-17 17:38:21 +0800

    '.'

--- app/src/main/java/com/zero/healthmonitoring/provide/Config.kt
+++ app/src/main/java/com/zero/healthmonitoring/provide/Config.kt
@@ -3,7 +3,7 @@ package com.zero.healthmonitoring.provide
 class Config {
 
     companion object{
-        const val HOST = "http://yiruizhaobiao.com/"//""http://bxu2404490132.my3w.com/"//""http://101.201.53.157/"
+        const val HOST = "http://c.shyrty.com/"//""http://yiruizhaobiao.com/"//""http://101.201.53.157/"
     }
 
 }
